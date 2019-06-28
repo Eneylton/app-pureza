f455faae87e1c1517178b0b32f725296c4246a34
eneylton <eneylton@hotmail.com>
2019-06-28 16:35:24 -0300

    Ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,43 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-
+import { CartPage } from '../pages/cart/cart';
+import { WelcomePage } from '../pages/welcome/welcome'
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HttpModule } from '@angular/http';
+import { TabsPage }  from '../pages/tabs/tabs'
+import { PayPal } from '@ionic-native/paypal';
+import { IonicStorageModule } from '@ionic/storage';
+import { WoocommerceProvider } from '../providers/woocommerce/woocommerce';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    CartPage,
+    TabsPage,
+    WelcomePage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+    CartPage,
+    TabsPage,
+    WelcomePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    PayPal,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    WoocommerceProvider
   ]
 })
 export class AppModule {}
